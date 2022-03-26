c09f673ecfedf897f8fa1ec19d6dfc2bbf565697
zixuan1986 <litthy@qq.com>
2022-03-26 10:12:10 +0800

    feat:专题页

--- src/pages/shengdianyunqi/App.vue
+++ src/pages/shengdianyunqi/App.vue
@@ -1,41 +1,41 @@
 <template>
-    <!-- 专题页 -->
-    <div class="p-topic" :class="'v-' + page_name">
-        <Header :overlayEnable="true"></Header>
-        <router-view></router-view>
-        <div class="p-topic-footer">
-            <div class="wp">
-                <Footer></Footer>
-            </div>
-        </div>
-    </div>
+	<!-- 专题页 -->
+	<div class="p-topic" :class="'v-' + page_name">
+		<Header :overlayEnable="true"></Header>
+		<router-view></router-view>
+		<div class="p-topic-footer">
+			<div class="wp">
+				<Footer></Footer>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import { postStat } from "@jx3box/jx3box-common/js/stat";
-import { __imgPath } from '@jx3box/jx3box-common/data/jx3box.json'
+import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
 export default {
-    name: "App",
-    props: [],
-    components: {},
-    data: function() {
-        return {};
-    },
-    provide : {
-        '__imgRoot' : __imgPath + 'topic/shengdianyunqi/'
-    },
-    computed: {
-        page_name: function() {
-            return this.$route.name;
-        },
-    },
-    watch: {},
-    methods: {},
-    filters: {},
-    created: function() {
-        postStat("topic", "shengdianyunqi");
-    },
-    mounted: function() {},
+	name: "App",
+	props: [],
+	components: {},
+	data: function () {
+		return {};
+	},
+	provide: {
+		__imgRoot: __imgPath + "topic/shengdianyunqi/",
+	},
+	computed: {
+		page_name: function () {
+			return this.$route.name;
+		},
+	},
+	watch: {},
+	methods: {},
+	filters: {},
+	created: function () {
+		postStat("topic", "shengdianyunqi");
+	},
+	mounted: function () {},
 };
 </script>
 
