df772d1b5d14edee3fd5b91219104ab377099b09
zixuan1986 <litthy@qq.com>
2022-03-30 20:40:30 +0800

    fix：替换图片及调整入口背景图片

--- src/components/index/Nav.vue
+++ src/components/index/Nav.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="m-topic-nav" :class="`${new_topic}-box`">
-        <img class="m-logo p-animations" :class="[animation ? 'fadeInLeft' : '', screenWidth < 1600 ? 'small' : '']" :src="logoImg" :alt="new_topic" srcset="" />
-        <img class="m-bg p-animations" :class="animation ? 'fadeInHalf' : ''" :src="boxBG" :alt="new_topic" srcset="" />
+        <div class="m-left">
+            <img class="m-bg p-animations" :class="animation ? 'fadeInHalf' : ''" :src="boxBG" :alt="new_topic" />
+            <!-- <div class="m-bg  p-animations" :class="animation ? 'fadeInHalf' : ''" :style="`background-image:url(${boxBG})`"></div> -->
+            <img class="m-logo p-animations" :class="[animation ? 'fadeInLeft' : '', screenWidth < 1600 ? 'small' : '']" :src="logoImg" :alt="new_topic" srcset="" />
+        </div>
+
         <div class="m-tabs">
-            <img class="m-people p-animations" :class="[animation ? 'fadeInRight' : '', new_topic]" :src="peopleImg" alt="人物" srcset="" />
+            <img class="m-people p-animations" :class="[animation ? 'fadeInRight' : '', new_topic]" :src="peopleImg" alt="人物"  />
             <div class="u-tab">
                 <span :class="[item.key == key ? 'active' : '', screenWidth < 1600 ? 'small' : '']" v-for="(item, index) in tabs" :key="index" @click="changeTabs(index)">{{ item.name }}</span>
             </div>
@@ -18,106 +22,108 @@
     </div>
 </template>
 <script>
-    import { new_topic, std_topic, origin_topic } from "@/assets/data/topic.json";
-    import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
-    export default {
-        name: "demo",
-        data: function() {
-            return {
-                new_topic,
-                std_topic,
-                origin_topic,
+import { new_topic, std_topic, origin_topic } from "@/assets/data/topic.json";
+import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
+export default {
+    name: "demo",
+    data: function () {
+        return {
+            new_topic,
+            std_topic,
+            origin_topic,
 
-                key: "all",
-                animation: true,
-                screenWidth: document.body.clientWidth,
-                showing: false,
-            };
+            key: "all",
+            animation: true,
+            screenWidth: document.body.clientWidth,
+            showing: false,
+        };
+    },
+    computed: {
+        tabs() {
+            return [
+                { key: "all", name: "全部", topic: this.new_topic },
+                { key: "std", name: "正式服", topic: this.std_topic[0].name },
+                { key: "origin", name: "怀旧服", topic: this.origin_topic[0].name },
+            ];
         },
-        computed: {
-            tabs() {
-                return [
-                    { key: "all", name: "全部", topic: this.new_topic },
-                    { key: "std", name: "正式服", topic: this.std_topic[0].name },
-                    { key: "origin", name: "怀旧服", topic: this.origin_topic[0].name },
-                ];
-            },
-            topic() {
-                let std_server = this.std_topic.map((item) => item.name);
-                let origin_server = this.origin_topic.map((item) => item.name);
-                let all_server = [...this.std_topic, ...this.origin_topic].sort((a, b) => b.id - a.id).map((item) => item.name);
-                return [
-                    { client: "all", server: all_server },
-                    { client: "std", server: std_server },
-                    { client: "origin", server: origin_server },
-                ];
-            },
-            boxBG: function() {
-                return __imgPath + "image/topic/" + this.new_topic + "_bg.jpg";
-            },
-            logoImg: function() {
-                return __imgPath + "image/topic/" + this.new_topic + "_logo.png";
-            },
-            peopleImg: function() {
-                return __imgPath + "image/topic/" + this.new_topic + "_people.png";
-            },
+        topic() {
+            let std_server = this.std_topic.map((item) => item.name);
+            let origin_server = this.origin_topic.map((item) => item.name);
+            let all_server = [...this.std_topic, ...this.origin_topic]
+                .sort((a, b) => b.id - a.id)
+                .map((item) => item.name);
+            return [
+                { client: "all", server: all_server },
+                { client: "std", server: std_server },
+                { client: "origin", server: origin_server },
+            ];
         },
+        boxBG: function () {
+            return __imgPath + "image/topic/" + this.new_topic + "_bg.jpg";
+        },
+        logoImg: function () {
+            return __imgPath + "image/topic/" + this.new_topic + "_logo.png";
+        },
+        peopleImg: function () {
+            return __imgPath + "image/topic/" + this.new_topic + "_people.png";
+        },
+    },
 
-        methods: {
-            // 切换tabs
-            changeTabs(index) {
-                this.new_topic = this.tabs[index].topic;
-                this.key = this.tabs[index].key;
+    methods: {
+        // 切换tabs
+        changeTabs(index) {
+            this.new_topic = this.tabs[index].topic;
+            this.key = this.tabs[index].key;
+            this.showAnimation();
+        },
+        imgStyle(img) {
+            return this.screenWidth < 1600
+                ? {
+                      backgroundImage: "url(" + __imgPath + "image/topic/" + img + "_smalltab.jpg)",
+                  }
+                : {
+                      backgroundImage: "url(" + __imgPath + "image/topic/" + img + "_bigtab.jpg)",
+                  };
+        },
+        // 跳转
+        toTopic(key) {
+            let prefix = process.env.NODE_ENV == "development" ? "/" : "/topic/";
+            return prefix + key;
+        },
+        // 动画效果
+        showAnimation() {
+            this.animation = false;
+            setTimeout(() => {
+                this.animation = true;
+            }, 10);
+        },
+        // 鼠标移入
+        enter(img) {
+            if (this.new_topic !== img) {
+                this.new_topic = img;
                 this.showAnimation();
-            },
-            imgStyle(img) {
-                return this.screenWidth < 1600
-                    ? {
-                          backgroundImage: "url(" + __imgPath + "image/topic/" + img + "_smalltab.jpg)",
-                      }
-                    : {
-                          backgroundImage: "url(" + __imgPath + "image/topic/" + img + "_bigtab.jpg)",
-                      };
-            },
-            // 跳转
-            toTopic(key) {
-				let prefix = process.env.NODE_ENV == 'development'? '/' : '/topic/'
-                return prefix + key;
-            },
-            // 动画效果
-            showAnimation() {
-                this.animation = false;
-                setTimeout(() => {
-                    this.animation = true;
-                }, 10);
-            },
-            // 鼠标移入
-            enter(img) {
-                if (this.new_topic !== img) {
-                    this.new_topic = img;
-                    this.showAnimation();
-                }
-            },
+            }
         },
-        watch: {
-            screenWidth: {
-                handler: function(val) {
-                    this.screenWidth = val;
-                },
-                immediate: true,
+    },
+    watch: {
+        screenWidth: {
+            handler: function (val) {
+                this.screenWidth = val;
             },
+            immediate: true,
         },
-        created: function() {},
-        mounted() {
-            const that = this;
-            window.onresize = () => {
-                return (() => {
-                    window.screenWidth = document.body.clientWidth;
-                    that.screenWidth = window.screenWidth;
-                })();
-            };
-        },
-    };
+    },
+    created: function () {},
+    mounted() {
+        const that = this;
+        window.onresize = () => {
+            return (() => {
+                window.screenWidth = document.body.clientWidth;
+                that.screenWidth = window.screenWidth;
+            })();
+        };
+    },
+};
 </script>
 <style lang="less">
     @import "~@/assets/css/index/nav.less";
