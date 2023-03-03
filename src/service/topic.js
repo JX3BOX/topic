b026063a4f2a751ffe096fbc0481092b8ead8345
yumg <mail>
2023-03-04 00:04:47 +0800

    妖秀节

--- src/service/topic.js
+++ src/service/topic.js
@@ -5,13 +5,28 @@ function getTopic(topic) {
         params: {
             type: topic,
             all: 1,
-            status : 1,
+            status: 1,
         },
     });
 }
 
-function getFbRank(id, server = '') {
-    return $team({ mute: true }).get(`/api/team/race/achieve/${id}/top100?server=${server}&event_id=1`)
+function getFbRank(id, server = "") {
+    return $team({ mute: true }).get(`/api/team/race/achieve/${id}/top100?server=${server}&event_id=1`);
+}
+function getToken() {
+    return $team().get("/api/team/role-bind-token");
+}
+function getRoles(params) {
+    return $team().get("/api/team/my-game-roles", {
+        params: params,
+    });
 }
 
-export { getTopic, getFbRank };
+function getAllMyRoles() {
+    return $team().get(`/api/team/my-game-roles?nopage`);
+}
+function getHonorApi(eventName, params) {
+    // 0 正确 非0 错误
+    return $team().post(`/api/team/website/events/${eventName}/collect-rewards`, params);
+}
+export { getTopic, getFbRank, getToken, getHonorApi, getRoles, getAllMyRoles };
