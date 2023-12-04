7ad341c8733161a3134681d33840cbd8a9d4aaa3
xuan <litthy@qq.com>
2023-12-04 23:34:55 +0800

    fix: api

--- src/service/topic.js
+++ src/service/topic.js
@@ -4,7 +4,6 @@ function getTopic(topic) {
     return $cms({ mute: true }).get("/api/cms/topic", {
         params: {
             type: topic,
-            all: 1,
             status: 1,
         },
     });
